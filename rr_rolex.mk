e551a1fa7a7059c89b3e7f5a20dd61ab163b34fd
Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>
2019-02-01 00:05:32 +0700

    rolex: RR-ify
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

--- rr_rolex.mk
+++ rr_rolex.mk
@@ -22,14 +22,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/rolex/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MODEL := Redmi 4A
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := rr_rolex
+RR_BUILDTYPE := Official
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
