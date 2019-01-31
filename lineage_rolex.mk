9b33b29e0751dc5c4fc429b78726e88aaa085bad
Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>
2019-01-31 23:57:02 +0700

    rolex: I like this way
    coz, why not?
    
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

--- lineage_rolex.mk
+++ lineage_rolex.mk
@@ -14,19 +14,31 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from rolex device
+$(call inherit-product, device/xiaomi/rolex/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := rolex
+PRODUCT_MODEL := Redmi 4A
+PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := lineage_rolex
-BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+TARGET_VENDOR_PRODUCT_NAME := rolex
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="rolex" \
+    PRODUCT_NAME="rolex" \
     PRIVATE_BUILD_DESC="rolex-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
 
 # Set BUILD_FINGERPRINT variable
 BUILD_FINGERPRINT := "Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys"
-
